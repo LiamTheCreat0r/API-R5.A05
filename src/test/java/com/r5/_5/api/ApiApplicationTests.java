50edbee3fb7573055985afc5705804b2db1a5146
Liam <106878018+LiamTheCreat0r@users.noreply.github.com>
2026-09-14 10:35:05 +0200

    hello-world

---
+++ src/test/java/com/r5/_5/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.r5._5.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
